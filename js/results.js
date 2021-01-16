94cb7f319fd64d8d819f63e16bece2ed0d8a724f
Kyle Welfare <kyledwelfare@gmail.com>
2021-01-16 15:20:08 -0500

    with js variables

--- js/results.js
+++ js/results.js
@@ -2,4 +2,27 @@ const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString);
 
-console.log(queryString);
+//universal variables
+const numParticipants = urlParams.get("participants");
+const ageRange = urlParams.get("age_range");
+
+// category checks
+const wantsGames = urlParams.has("games");
+const wantsExercises = urlParams.has("exercises");
+const wantsCooking = urlParams.has("cooking");
+const wantsMovTv = urlParams.has("mov-tv");
+
+//game variables
+const gameGenres = urlParams.getAll("game-genres");
+const costRange = urlParams.get("cost_range");
+
+//exercises variables
+const exerTypes = urlParams.getAll("exer-types");
+
+//cooking variables
+const mealTypes = urlParams.getAll("meal-types");
+
+//mov-tv variables
+const movTvGenres = urlParams.getAll("mov-tv-genres");
+
+// console.log(queryString);
