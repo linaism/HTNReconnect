d01c76acd6046107514ce5e4638449ccac46692d
Kyle Welfare <kyledwelfare@gmail.com>
2021-01-16 14:36:51 -0500

    01/16/2021

--- js/app.js
+++
@@ -1,6 +0,0 @@
-function saveURLParams() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams;
-}
-
-// if (window.location.href === "../")
---
+++ js/results.js
@@ -0,0 +1,5 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+console.log(queryString);
