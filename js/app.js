26d777f3e62af15a69b30b8e23c9a0b54025bead
Kyle Welfare <kyledwelfare@gmail.com>
2021-01-16 14:04:14 -0500

    01/16/2021

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function saveURLParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams;
+}
+
+// if (window.location.href === "../")
---
+++ js/jquery.js
@@ -0,0 +1,21 @@
+$("#js-range-slider").ionRangeSlider({
+  min: 0,
+  max: 60,
+  from: 8,
+  to: 25,
+  type: "double",
+  max_postfix: "+",
+  postfix: " years",
+  skin: "flat",
+});
+
+$("#js-range-slider2").ionRangeSlider({
+  min: 0,
+  max: 60,
+  from: 8,
+  to: 25,
+  type: "double",
+  max_postfix: "+",
+  prefix: "$",
+  skin: "flat",
+});
